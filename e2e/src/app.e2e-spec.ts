4b01f598a8c306101692c842e22efd39ec85556a
thehunckerma <thehunckerma@hotmail.com>
2021-06-20 16:23:10 +0100

    Minor changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dot_env_angular app is running!');
+    expect(page.getTitleText()).toEqual('dot_net_angular app is running!');
   });
 
   afterEach(async () => {
