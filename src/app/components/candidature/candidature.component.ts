1b102743542e7f92ad2f5e358b77e16d36a10988
thehunckerma <thehunckerma@hotmail.com>
2021-06-21 17:16:46 +0100

    Complete candidature frontend (no integration yet)

--- src/app/components/candidature/candidature.component.ts
+++ src/app/components/candidature/candidature.component.ts
@@ -12,8 +12,21 @@ interface Justificatif {
   styleUrls: ['./candidature.component.css'],
 })
 export class CandidatureComponent implements OnInit {
-  gradeControl = new FormControl('', Validators.required);
   grades = [{ name: 'A' }, { name: 'B' }, { name: 'C' }, { name: 'D' }];
+  cadreGrade = [
+    { name: '[PES] Grade A à grade B', value: 'PESAB' },
+    { name: '[PES] Grade B à grade C', value: 'PESBC' },
+    { name: '[PH] Grade A à grade B', value: 'PHAB' },
+    { name: '[PH] Grade B à grade C', value: 'PHBC' },
+    { name: '[PA] Grade A à grade B', value: 'PAAB' },
+    { name: '[PA] Grade B à grade C', value: 'PABC' },
+    { name: '[PA] Grade C à grade D', value: 'PACD' },
+  ];
+  typeAvancement = [
+    { name: 'Passage Exceptionnel', value: 'E' },
+    { name: 'Passage Rapide', value: 'R' },
+    { name: 'Passage Normal', value: 'N' },
+  ];
 
   donneesPersonnelles = new FormGroup({
     nom: new FormControl('', Validators.required),
@@ -29,16 +42,40 @@ export class CandidatureComponent implements OnInit {
 
   passageCadreGrade = new FormGroup({
     typePassage: new FormControl('', Validators.required),
+    cadreGrade: new FormControl('', Validators.required),
   });
 
-  ouvragesOuManuels: Array<Justificatif> = [];
+  ouvragesManuels: Array<Justificatif> = [];
   polycopiesEnseignement: Array<Justificatif> = [];
-  supportsEtProcedesDidactiques: Array<Justificatif> = [];
+  supportsProcedesDidactiques: Array<Justificatif> = [];
   encadrementProjetsPFE: Array<Justificatif> = [];
   encadrementProjetsPFA: Array<Justificatif> = [];
-  memoiresdeFinEtudesMaster: Array<Justificatif> = [];
-  encadrementDeStages: Array<Justificatif> = [];
-  encadrementDeRessourcesHumaines: Array<Justificatif> = [];
+  memoiresFinEtudesMaster: Array<Justificatif> = [];
+  encadrementStages: Array<Justificatif> = [];
+  encadrementRessourcesHumaines: Array<Justificatif> = [];
+  gestionDepartement: Array<Justificatif> = [];
+  responsableFiliereModule: Array<Justificatif> = [];
+  gestionFormationUniversitairePrepa: Array<Justificatif> = [];
+  gestionFormationUniversitaireIng: Array<Justificatif> = [];
+  membreConseilUniversite: Array<Justificatif> = [];
+  membreCommissionParitaire: Array<Justificatif> = [];
+  membreConseilEtablissement: Array<Justificatif> = [];
+  membreCommissionScientifique: Array<Justificatif> = [];
+  membreCommissionReformeExpertisePedagogique: Array<Justificatif> = [];
+  articlesScientifiquesCategorie1: Array<Justificatif> = [];
+  articlesScientifiquesCategorie2: Array<Justificatif> = [];
+  ouvragesRechercheScientifiques: Array<Justificatif> = [];
+  publicationsActesCongres: Array<Justificatif> = [];
+  encadrementCoencadrementThesesDoctorat: Array<Justificatif> = [];
+  encadrementCoencadrementTravauxRechercheMaster: Array<Justificatif> = [];
+  RapporteurMembreJury: Array<Justificatif> = [];
+  laboratoireEquipeCed: Array<Justificatif> = [];
+  polesCompetenceReseaux: Array<Justificatif> = [];
+  cedDoctoratMaster: Array<Justificatif> = [];
+  projetsContratsRechercheFinances: Array<Justificatif> = [];
+  activitesExpertiseEvaluationsScientifiques: Array<Justificatif> = [];
+  organisationManifestationsScientifiques: Array<Justificatif> = [];
+  organisationSeminaire: Array<Justificatif> = [];
 
   constructor() {}
 
